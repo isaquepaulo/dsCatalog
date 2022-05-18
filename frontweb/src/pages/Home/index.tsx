864bce89a426bb26b0c1e81fff4b13139495a58f
Isaque de Paulo Gonçalves <isaquegoncalves031@gmail.com>
2022-05-18 12:46:37 -0300

    HomePage Criada

---
+++ frontweb/src/pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import Navbar from "components/Navbar";
+import { ReactComponent as MainImage } from "assets/images/Desenho.svg";
+import "./styles.css";
+
+import ButtonIcon from "components/ButtonIcon";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-content-container">
+            <div>
+              <h1>Conheça o melhor catalogo de produtos</h1>
+              <p>
+                Ajudaremos você a encontrar os melhores produtos disponíveis no
+                mercado.
+              </p>
+            </div>
+
+            <ButtonIcon />
+          </div>
+          <div className="home-image-container">
+            <MainImage />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
