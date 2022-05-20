6477f3e7a89caaa58924e05f81630226691408db
Isaque de Paulo Gonçalves <isaquegoncalves031@gmail.com>
2022-05-20 16:32:16 -0300

    ProductsDetail links

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,15 +1,18 @@
 import "./styles.css";
 import { ReactComponent as ArrowIcons } from "assets/images/arrow.svg";
 import ProductPrice from "components/ProductPrice";
+import { Link } from "react-router-dom";
 
 const ProductDetails = () => {
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
-        <div className="goback-container">
-          <ArrowIcons />
-          <h2>Voltar</h2>
-        </div>
+        <Link to="/products">
+          <div className="goback-container">
+            <ArrowIcons />
+            <h2>Voltar</h2>
+          </div>
+        </Link>
         <div className="row">
           <div className="col-xl-6">
             <div className="img-container">
