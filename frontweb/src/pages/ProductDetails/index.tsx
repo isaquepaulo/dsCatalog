25ef55ff391c1cdaa994b54931ed1094f39c7082
Isaque de Paulo Gonçalves <isaquegoncalves031@gmail.com>
2022-05-21 17:09:27 -0300

    usestate, useEffect, useparams

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,9 +1,26 @@
 import "./styles.css";
 import { ReactComponent as ArrowIcons } from "assets/images/arrow.svg";
 import ProductPrice from "components/ProductPrice";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { Product } from "types/product";
+import axios from "axios";
+import { BASE_URL } from "util/request";
+import { useEffect, useState } from "react";
 
+type UrlParams = {
+  productId: string;
+};
 const ProductDetails = () => {
+  const { productId } = useParams<UrlParams>();
+
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/products/${productId}`).then((response) => {
+      setProduct(response.data);
+    });
+  }, [productId]);
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
@@ -16,23 +33,17 @@ const ProductDetails = () => {
         <div className="row">
           <div className="col-xl-6">
             <div className="img-container">
-              <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-                alt="Nome produto"
-              />
+              <img src={product?.imgUrl} alt={product?.name} />
             </div>
             <div className="name-price-container d-xl-block">
-              <h1>Nome do Produto</h1>
-              <ProductPrice price={2345.67} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do produto</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis,
-                eligendi?
-              </p>
+              <p>{product?.description}</p>
             </div>
           </div>
         </div>
---
+++ frontweb/src/util/request.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = process.env.React_APP_BACKEND_URL ?? 'http://localhost:8080';
+
