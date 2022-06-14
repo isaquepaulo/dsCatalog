64d6989c45eb5f242bbb1c26df0395b14e916cf4
Isaque de Paulo Gonçalves <isaquegoncalves031@gmail.com>
2022-06-14 11:38:15 -0300

    Login/logout logic

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,7 +1,45 @@
 import "./styles.css";
 import "bootstrap/js/src/collapse.js";
 import { Link, NavLink } from "react-router-dom";
+import {
+  getTokenData,
+  isAuthenticated,
+  removeAuthData,
+  TokenData,
+} from "util/request";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import history from "util/history";
+
+type AuthData = {
+  authenticated: boolean;
+  tokenData?: TokenData;
+};
+
 const Navbar = () => {
+  const [authData, setAuthData] = useState<AuthData>({ authenticated: false });
+  useEffect(() => {
+    if (isAuthenticated()) {
+      setAuthData({
+        authenticated: true,
+        tokenData: getTokenData(),
+      });
+    } else {
+      setAuthData({
+        authenticated: false,
+      });
+    }
+  }, []);
+
+  const handleLogoutClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    removeAuthData();
+    setAuthData({
+      authenticated: false,
+    });
+    history.replace("/");
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
@@ -46,6 +84,19 @@ const Navbar = () => {
                 ADMIN
               </NavLink>
             </ul>
+          </div>
+
+          <div>
+            {authData.authenticated ? (
+              <>
+                <span>{authData.tokenData?.user_name}</span>
+                <a href="#logout" onClick={handleLogoutClick}>
+                  Logout
+                </a>
+              </>
+            ) : (
+              <Link to="/admin/auth">Login</Link>
+            )}
           </div>
         </div>
       </nav>
--- frontweb/src/util/request.ts
+++ frontweb/src/util/request.ts
@@ -4,7 +4,8 @@ import history from './history';
 import jwtDecode from 'jwt-decode'
 
 type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
-type TokenData = 
+
+export type TokenData = 
     {
         exp: number,
         user_name: string,
@@ -63,6 +64,10 @@ export const getAuthData = () => {
     const str = localStorage.getItem(tokenKey) ?? "{}";
     const obj = JSON.parse(str);
     return obj as LoginResponse;
+}
+
+export const removeAuthData = () => {
+    localStorage.removeItem(tokenKey);
 }
 
 // Add a request interceptor
