b8e5678d5874787d1887dedb846199531d0fc925
Isaque de Paulo Gonçalves <isaquegoncalves031@gmail.com>
2022-06-20 11:24:05 -0300

    CategoryBadge

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,24 +1,24 @@
-import ProductCrudCard from "components/ProductCrudCard";
+import ProductCrudCard from "pages/Admin/Products/ProductCrudCard";
 import { Link } from "react-router-dom";
 import "./styles.css";
 
 const List = () => {
   const product = {
-    id: 2,
-    name: "Smart TV",
+    id: 1,
+    name: "The Lord of the Rings",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    price: 2190.0,
+    price: 90.5,
     imgUrl:
-      "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
-    date: "2020-07-14T10:00:00Z",
+      "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg",
+    date: "2020-07-13T20:50:07.123450Z",
     categories: [
       {
-        id: 1,
-        name: "Livros",
+        id: 2,
+        name: "Eletrônicos",
       },
       {
-        id: 3,
+        id: 1,
         name: "Computadores",
       },
     ],
