7bfa6bc8ea59dc823330a6d102d81949d3c9b08f
Isaque de Paulo Gonçalves <isaquegoncalves031@gmail.com>
2022-06-22 11:43:13 -0300

    React Select layout

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -2,6 +2,7 @@ import { AxiosRequestConfig } from "axios";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate, useParams } from "react-router-dom";
+import Select from "react-select";
 import { Product } from "types/product";
 import { requestBackend } from "util/request";
 
@@ -12,6 +13,12 @@ type UrlParams = {
 };
 
 const Form = () => {
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
+
   const { productId } = useParams<UrlParams>();
 
   const isEditing = productId !== "create";
@@ -85,6 +92,15 @@ const Form = () => {
                   {errors.name?.message}
                 </div>
               </div>
+
+              <div className="margin-bottom-30">
+                <Select
+                  options={options}
+                  classNamePrefix="product-crud-select"
+                  isMulti
+                />
+              </div>
+
               <div className="margin-bottom-30">
                 <input
                   {...register("price", {
