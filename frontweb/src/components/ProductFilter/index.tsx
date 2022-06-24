dd690513be0a75ba3e5ce68e730734366998004c
Isaque de Paulo Gonçalves <isaquegoncalves031@gmail.com>
2022-06-24 14:10:55 -0300

    ProductFilter internal behavior

--- frontweb/src/components/ProductFilter/index.tsx
+++ frontweb/src/components/ProductFilter/index.tsx
@@ -8,17 +8,34 @@ import "./styles.css";
 
 type ProductFilterData = {
   name: string;
-  category: Category;
+  category: Category | null;
 };
 
 const ProductFilter = () => {
   const [selectCategories, setSelectCategories] = useState<Category[]>([]);
 
-  const { register, handleSubmit, control } = useForm<ProductFilterData>();
+  const { register, handleSubmit, setValue, getValues, control } =
+    useForm<ProductFilterData>();
 
   const onSubmit = (formData: ProductFilterData) => {
     console.log("Enviou", formData);
   };
+
+  const handleFormClear = () => {
+    setValue("name", "");
+    setValue("category", null);
+  };
+
+  const handleChangeCategory = (value: Category) =>{
+    setValue('category',value);
+
+    const obj : ProductFilterData = { 
+        name: getValues('name'),
+        category: getValues('category'),
+    }
+    console.log("Enviou", obj);
+
+  }
 
   useEffect(() => {
     requestBackend({ url: "/categories" }).then((response) => {
@@ -53,13 +70,17 @@ const ProductFilter = () => {
                   isClearable
                   placeholder="Categoria"
                   classNamePrefix="product-filter-select"
+                  onChange={value => handleChangeCategory(value as Category)}
                   getOptionLabel={(category: Category) => category.name}
                   getOptionValue={(category: Category) => String(category.id)}
                 />
               )}
             />
           </div>
-          <button className="btn btn-outline-secondary btn-product-filter-clear">
+          <button
+            onClick={handleFormClear}
+            className="btn btn-outline-secondary btn-product-filter-clear"
+          >
             Limpar<span className="btn-product-filter-word"> Filtro</span>{" "}
           </button>
         </div>
