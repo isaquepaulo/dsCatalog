1ee8d0ee95f28c408fb351c6c837fd8d73ecfe08
Isaque de Paulo <isaquegoncalves031@gmail.com>
2022-06-30 11:43:06 -0300

    First test

---
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -0,0 +1,11 @@
+import { formatPrice } from "util/formatters"
+
+test('formatPrice should format number pt-BR when given 10.1', () => {
+    // ARRANGE
+    const value = 10.1;
+    // ACT
+    const result = formatPrice(value);
+    // ASSERT
+    expect(result).toEqual("10,10");
+
+})
